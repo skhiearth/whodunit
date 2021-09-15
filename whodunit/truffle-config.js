2346b1deb84be336f4864adcbc92ff29c6dbaef0
Utkarsh Sharma <skhiearth@gmail.com>
2021-09-15 19:44:54 +0530

    mnemonic

--- whodunit/truffle-config.js
+++ whodunit/truffle-config.js
@@ -1,5 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+import { MNEMONIC }  from './secret.js'
 
 module.exports = {
   networks: {
@@ -8,6 +11,20 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    dev: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: MNEMONIC,
+          },
+          providerOrUrl: "https://babel-api.testnet.iotex.io",
+          shareNonce: true
+        }),
+      network_id: 4690, // IOTEX mainnet chain id 4689, testnet is 4690
+      gas: 8500000,
+      gasPrice: 1000000000000,
+      skipDryRun: true
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
